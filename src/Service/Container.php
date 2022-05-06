31f99d310eae8ba1f27cf11f0e6193747d2e5ca4
Celie Russier <celie.russier@gmail.com>
2022-05-06 13:29:55 +0200

    last commit

--- src/Service/Container.php
+++ src/Service/Container.php
@@ -4,4 +4,27 @@ namespace App\Service;
 
 class Container
 {
+    public function inbox(int $numberCake): array
+    {
+        $smallBoxes = 0;
+        $mediumBoxes = 0;
+        $largeBoxes = 0;
+        $numberOfBoxes = [$smallBoxes, $mediumBoxes, $largeBoxes];
+        if ($numberCake < 3) {
+            $smallBoxes = 1;
+        }
+        if ($numberCake < 6 && $numberCake > 2) {
+            $mediumBoxes = 1;
+        }
+        if ($numberCake > 5 && $numberCake < 9) {
+            $largeBoxes = 1;
+        }
+        if ($numberCake > 8) {
+            $largeBoxes = (int) ($numberCake / 8);
+            $additionalCakes = $numberCake % 8;
+            $mediumBoxes = (int) ($additionalCakes / 5);
+            // time'sup :(
+        }
+        return $numberOfBoxes;
+    }
 }
